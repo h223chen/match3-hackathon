c8f1dce69242d312f365797876452bc50947e4c7
hchung@zynga.com <johndoe@example.com>
2017-10-05 14:17:55 -0400

    real time game handler

---
+++ realTimeGameHandler.js
@@ -0,0 +1,25 @@
+class RealTimeGameHandler {
+
+	constructor(room) {
+		// hack to get gamehandler fields within socket scope
+		var game = this;
+		// initialize game fields
+		this.testCount = 0;
+
+		// game logic
+		room.users.forEach(function(user) {
+			var socket = user.socket;
+
+			// initialize game
+			socket.emit('gameStart', 'gameStart');
+
+
+			// test function			
+			socket.on('whatGameRoom', function() {
+				socket.emit('whatGameRoom', {"room" : room.id, "testCount" : game.testCount++});
+			});
+		});
+	}
+}
+
+module.exports = RealTimeGameHandler;
