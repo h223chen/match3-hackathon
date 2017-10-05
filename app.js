d9d3f91d9575239e1d2f8decb0cefc115e1d509e
hchung@zynga.com <johndoe@example.com>
2017-10-05 11:28:20 -0400

    boilerplate rooms

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-
 app.use('/assets', express.static(__dirname + '/assets'));
-
 var fs = require('fs');
+
+// constants
+const ROOM_SIZE = 2;
+
+var rooms = [];
+var matchmakingQueue = [];
+var connectedSocketIds = [];
+
+function setupConnection(socket) {
+	connectedSocketIds.push(socket.id);
+	console.log(socket.id + " connected.");
+
+	// add to queue
+	matchmakingQueue.push(socket.id);
+	if (matchmakingQueue.length >= ROOM_SIZE) {
+		matchmakingQueue.splice(0, ROOM_SIZE);
+	}
+
+	// disconnect logic
+	socket.on('disconnect', function() {
+	  console.log(socket.id + " disconnected.");
+	  connectedSocketIds.splice(connectedSocketIds.indexOf(socket.id), 1);
+	});
+}
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
@@ -16,6 +38,8 @@ server.listen(process.env.PORT || 8081,function(){
 });
 
 io.on('connection', function (socket) {
+	setupConnection(socket);
+
   socket.on('move', function (data) {
     console.log(data);
     socket.emit('clientReceive', {message: "server received your move"});
---
+++ room.js
@@ -0,0 +1,7 @@
+class Room {
+	constructor(users) {
+		this.users;
+	}	
+};
+
+module.exports = Room;
