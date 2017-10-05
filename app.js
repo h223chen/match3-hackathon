3092b29f4238a33f0258d04e505a84f93bdc2cb1
Haotian Chen <haochen@zynga.com>
2017-10-05 11:47:57 -0400

    fixed merged conflicts

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var os = require('os');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+
 app.use('/assets', express.static(__dirname + '/assets'));
 var fs = require('fs');
 
@@ -30,13 +32,42 @@ function setupConnection(socket) {
 	});
 }
 
+var ifaces = os.networkInterfaces();
+app.use('/assets', express.static(__dirname + '/assets'));
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
 server.listen(process.env.PORT || 8081,function(){
-    console.log('Listening on '+server.address().port);
+    console.log('Listening on localhost:'+server.address().port);
 });
+
+// getting network interfaces
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log("Listening on (" + alias + ") " + iface.address + ":" + server.address().port);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log("Listening on " + iface.address + ":" + server.address().port);
+    }
+
+    ++alias;
+  });
+});
+
+// ============================================================
+
+
+
+
 
 io.on('connection', function (socket) {
 	setupConnection(socket);
